503d5c0127d734f825980d911d2c3a1a32446ab6
stepulik1 <leviychel56@gmail.com>
2025-12-10 22:01:49 +0500

    2 commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <string>
+
+void readLinesFromFile(const std::string& filename, std::vector<std::string>& lines) {
+}
+
+void printLinesToScreen(const std::vector<std::string>& lines) {
+}
+
+void writeLinesToFile(const std::string& filename, const std::vector<std::string>& lines) {
+}
 
 int main() {
-    cout << "Hello World!" << endl;
+    std::vector<std::string> myLines;
+    readLinesFromFile("input.txt", myLines);
+    printLinesToScreen(myLines);
+    writeLinesToFile("output.txt", myLines);
+
     return 0;
 }
