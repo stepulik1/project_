3622184e8e06038d456c00a0f45adc7a15b2db27
stepulik1 <leviychel56@gmail.com>
2025-12-12 17:52:14 +0500

    input function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 #include <fstream>
 
 void readLinesFromFile(const std::string& filename, std::vector<std::string>& lines) {
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Ошибка: Не удалось открыть файл " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line); 
+    }
+
+    file.close();
+    std::cout << "Чтение из файла завершено (" << lines.size() << " строк)" << std::endl;
 }
 
 void printLinesToScreen(const std::vector<std::string>& lines) {
